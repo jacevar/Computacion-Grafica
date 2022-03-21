5fafefadd6b2235cdb9385aad8f00584d9030334
jacevar <63611163+jacevar@users.noreply.github.com>
2022-03-21 13:53:59 -0500

    Delete ParcialGrafica directory

--- ParcialGrafica/CastelJau.js
+++
@@ -1,3 +0,0 @@
-let lerp = (t, p1, p2) => (1 - t) * p1 + t * p2;
-let reduce = (t, p1, p2, ...ps) => ps.length > 0 ? [lerp(t, p1, p2), ...reduce(t, p2, ...ps)] : [lerp(t, p1, p2)];
-let deCasteljau = (t, ps) => ps.length > 1 ? deCasteljau(t, reduce(t, ...ps)) : ps[0];
